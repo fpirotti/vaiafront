d8948c1a976516aab81bb57bf7f08101b2c5aab4
fpirotti <fpirotti@gmail.com>
2020-04-17 12:27:37 +0200

    Modifiche varie

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -16,14 +16,14 @@
 ## Run checks ----
 ## Check the package before sending to prod
 devtools::check()
-rhub::check_for_cran()
+#rhub::check_for_cran()
 
 # Deploy
 
 ## RStudio ----
 ## If you want to deploy on RStudio related platforms
-golem::add_rstudioconnect_file()
-golem::add_shinyappsio_file()
+#golem::add_rstudioconnect_file()
+#golem::add_shinyappsio_file()
 golem::add_shinyserver_file()
 
 ## Docker ----
